d1fa458c833123421e971dd99f8d237596f62ab1
the-fool <sketchbang@gmail.com>
2016-03-16 11:16:57 -0500

    add department course list

--- kgraph/departments/api.py
+++ kgraph/departments/api.py
@@ -3,6 +3,9 @@ from rest_framework import generics, permissions
 from .models import Department
 from .serializers import DepartmentSerializer
 
+from ..courses.models import Course
+from ..courses.serializers import CourseSerializer
+
 class DepartmentList(generics.ListAPIView):
     model = Department
     serializer_class = DepartmentSerializer
@@ -13,3 +16,14 @@ class DepartmentList(generics.ListAPIView):
     def get_queryset(self):
         queryset = Department.objects.all()
         return queryset
+
+class DepartmentCourseList(generics.ListAPIView):
+    model = Course
+    serializer_class = CourseSerializer
+    permission_classes = [
+        permissions.AllowAny
+    ]
+
+    def get_queryset(self):
+        queryset = Course.objects.select_related('department').all()
+        return queryset.filter(department__pk=self.kwargs.get('pk'))
--- kgraph/departments/urls.py
+++ kgraph/departments/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 
-from .api import DepartmentList
+from .api import DepartmentList, DepartmentCourseList
 
 department_urls = [
-    url(r'^$', DepartmentList.as_view(), name='department-list')
+    url(r'^$', DepartmentList.as_view(), name='department-list'),
+    url(r'^(?P<pk>[\w-]+)/courses/$', DepartmentCourseList.as_view(), name='departmentcourse-list'),
 ]
 
 urlpatterns = [
