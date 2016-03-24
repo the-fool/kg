9ebf50f0e26a625fd7580165cf07533460359a9d
the-fool <sketchbang@gmail.com>
2016-03-24 18:18:17 -0500

    add fields to course model

--- kgraph/courses/management/commands/create_courses.py
+++ kgraph/courses/management/commands/create_courses.py
@@ -1,3 +1,6 @@
+import random
+
+from django.db import IntegrityError, transaction
 from django.core.management.base import BaseCommand
 
 from kgraph.courses.models import Course
@@ -10,8 +13,17 @@ class Command(BaseCommand):
 
         departments = list(Department.objects.all())
         courses = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight']
-        
+
         for d in departments:
             for c1 in courses:
                 for c2 in courses:
-                    Course.objects.create(title="{}-{}".format(c1, c2), department=d)
+                    success = False
+                    while not success:
+                        course_number = random.randint(100, 999)
+                        course_tier = course_number % 100 + 1
+                        try:
+                            with transaction.atomic():
+                                Course.objects.create(title="{}-{}".format(c1, c2), department=d, course_number=course_number, tier=course_tier)
+                                success = True
+                        except IntegrityError:
+                            pass
--- kgraph/courses/models.py
+++ kgraph/courses/models.py
@@ -1,6 +1,8 @@
 import uuid
 
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 from kgraph.core.exceptions import CircularDependency
 from ..departments.models import Department
 
@@ -13,8 +15,13 @@ def get_default_department():
 
 class Course(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    course_number = models.IntegerField(default=0, validators=[MaxValueValidator(10000), MinValueValidator(1)])
+    tier = models.IntegerField(default=0, validators=[MaxValueValidator(16), MinValueValidator(1)])
     title = models.CharField(max_length=128, default="nameless")
     department = models.ForeignKey(Department, on_delete=models.SET_DEFAULT, null=True, default=None, db_index=True)
+
+    class Meta:
+        unique_together = (('department', 'course_number'), ('department','title'))
 
 class Edge(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
