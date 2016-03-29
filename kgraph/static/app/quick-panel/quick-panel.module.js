b7e7a202f9828722c0a5181d01e5cfeea55b00bb
the-fool <sketchbang@gmail.com>
2016-03-28 21:05:15 -0500

    add dummy data back to quick panel

--- kgraph/static/app/quick-panel/quick-panel.module.js
+++ kgraph/static/app/quick-panel/quick-panel.module.js
@@ -15,7 +15,7 @@
         // Api
         msApiProvider.register('quickPanel.course-detail', ['api/v1/courses/:id/']);
 
-        msApiProvider.register('quickPanel.activities', ['app/data/quick-panel/activities.json']);
+        msApiProvider.register('quickPanel.activities', ['static/app/data/quick-panel/activities.json']);
         msApiProvider.register('quickPanel.contacts', ['static/app/data/quick-panel/contacts.json']);
         msApiProvider.register('quickPanel.events', ['static/app/data/quick-panel/events.json']);
         msApiProvider.register('quickPanel.notes', ['static/app/data/quick-panel/notes.json']);
